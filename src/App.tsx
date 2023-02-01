02d758789374d6d329ed660555544ca2287857f0
mdunkel <mrks.dunkel@gmail.com>
2023-02-01 11:48:49 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Box, Tab, Tabs, Typography } from '@mui/material';
+import { SyntheticEvent, useRef, useState } from 'react';
 import './App.css';
+import ScrollToTop from './components/ScrollToTop';
+
+
 
 function App() {
+  const about: any = useRef<HTMLInputElement>(null);
+  const project: any = useRef<HTMLInputElement>(null);
+  const contact: any = useRef<HTMLInputElement>(null);
+
+  console.log("contact", contact);
+
+
+  const [tabIndex, setTabIndex] = useState('about');
+
+
+  const scrollToSection = (event: SyntheticEvent<Element, Event>, newTabIndex: string) => {
+    setTabIndex(newTabIndex);
+    console.log({ event });
+    console.log("newTabIndex", newTabIndex);
+
+    if (newTabIndex === 'about') {
+      window.scrollTo({
+        top: about.current.offsetTop,
+        behavior: "smooth",
+      });
+    } else if (newTabIndex === 'project') {
+      window.scrollTo({
+        top: project.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+    else {
+      window.scrollTo({
+        top: contact.current.offsetTop,
+        behavior: "smooth",
+      });
+    }
+  }
+  console.log("tabIndex", tabIndex);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <ScrollToTop />
+      <Tabs value={tabIndex} onChange={scrollToSection} centered>
+        <Tab value='about' label="About me" />
+        <Tab value='project' label="Projects" />
+        <Tab value='contact' label="Contact Me" />
+      </Tabs>
+      <Box sx={{ padding: 2 }}>
+
+        <Box height='500px' ref={about} id='about'>
+          <Typography>The first tab</Typography>
+        </Box>
+        <Box height='500px' ref={project} id='projects'>
+          <Typography>The second tab</Typography>
+        </Box>
+        <Box height='500px' ref={contact} id='contact'>
+          <Typography>The third tab</Typography>
+        </Box>
+
+      </Box>
+    </Box >
   );
 }
 
---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleUp } from "react-icons/fa";
+import { Box, Tab, Tabs, Typography } from '@mui/material';
+import { IconContext } from "react-icons";
+
+const ScrollToTop = () => {
+    const [showScrollTopButton, setShowScrollTopButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 0) {
+                setShowScrollTopButton(true);
+            } else {
+                setShowScrollTopButton(false);
+            }
+        });
+    }, []);
+
+    const scrollTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
+    return (
+        <IconContext.Provider value={{ color: "blue", size: 100, className: "global-class-name" }}>
+            <div>
+                {showScrollTopButton && (
+                    <Box sx={{ position: "fixed", bottom: 30, right: 30, m: 0, border: 'solid 1px orange' }}>
+                        <FaAngleDoubleUp
+                            onClick={scrollTop}
+                        />
+                    </Box>
+                )}
+            </div>
+        </IconContext.Provider>
+    )
+}
+
+export default ScrollToTop
